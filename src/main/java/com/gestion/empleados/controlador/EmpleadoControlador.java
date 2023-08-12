2ecd2dc05ee5031c981b3ed17928d02130ac954b
edinson <edinsonjaimeuriartecieza@gmail.com>
2023-08-12 00:52:07 -0500

    cambios

--- src/main/java/com/gestion/empleados/controlador/EmpleadoControlador.java
+++ src/main/java/com/gestion/empleados/controlador/EmpleadoControlador.java
@@ -3,6 +3,7 @@ package com.gestion.empleados.controlador;
 import com.gestion.empleados.modelo.Empleado;
 import com.gestion.empleados.repositorio.EmpleadoRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/")
+@CrossOrigin(origins = "http://localhost:4200")
 public class EmpleadoControlador {
 
     @Autowired
